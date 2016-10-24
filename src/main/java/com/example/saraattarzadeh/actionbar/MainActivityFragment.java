8658c6d5e9bd34b6c1196e0d684d4cb899f2b950
Sara Attarzadeh <saatta01@louisville.edu>
2016-10-24 15:37:39 -0400

    first commit for an action bar tutorial app

---
+++ src/main/java/com/example/saraattarzadeh/actionbar/MainActivityFragment.java
@@ -0,0 +1,21 @@
+package com.example.saraattarzadeh.actionbar;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}
